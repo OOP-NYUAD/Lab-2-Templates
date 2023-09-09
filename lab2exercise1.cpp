7b17e01c2532b311bc7b006cee9d5ea2f9b27b24
sqalli <tahrisqalli@gmail.com>
2023-09-09 15:38:09 +0400

    Create lab2exercise1.cpp

---
+++ lab2exercise1.cpp
@@ -0,0 +1,32 @@
+//This programming exercise is designed to help you practice creating a C++ class for a bank account.
+//The exercise involves creating an Account class with various functionalities, 
+//including setting and getting the balance, depositing and withdrawing funds,
+//and testing the class with a main function.
+//First, you would need to complete the "account.h" file before testing it using "lab2exercise1.cpp"    
+
+#include <iostream>
+#include "account.h"
+
+int main() {
+    // TODO: Create an Account object using the default constructor.
+    
+    // TODO: Create another Account object with an initial balance of $1000.
+
+    // TODO: Test the 'getBalance' function to retrieve the balance of both accounts and print them.
+
+    // TODO: Deposit $500 into the first account.
+
+    // TODO: Withdraw $200 from the second account.
+
+    // TODO: Test the 'getBalance' function again to check the updated balances and print them.
+
+    // TODO: Set a new balance of $3000 for the first account using the 'setBalance' function.
+
+    // TODO: Test the 'getBalance' function for the first account after setting the new balance.
+
+    // TODO: Test the 'withdraw' function for the second account, attempting to withdraw an amount greater than the balance.
+
+    // TODO: Test the destructor for both accounts.
+
+    return 0;
+}
